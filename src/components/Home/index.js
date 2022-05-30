0b3114bdf3f001a4ecdb2e706e8b500755397852
Ishan Shah <ishan0102@utexas.edu>
2022-05-30 16:17:26 -0700

    Fix allowFullScreen syntax

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -222,7 +222,7 @@ export function Home() {
           <SectionContainer>
             <SectionTitle>Music</SectionTitle>
             <SectionContent>
-              <iframe className="rounded-xl" src="https://open.spotify.com/embed/playlist/0y58YIsqNmmY5Bh3f4p0Vl?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+              <iframe className="rounded-xl" src="https://open.spotify.com/embed/playlist/0y58YIsqNmmY5Bh3f4p0Vl?utm_source=generator" width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
             </SectionContent>
           </SectionContainer>
         </div>
