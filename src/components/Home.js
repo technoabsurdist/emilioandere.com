802fbd732d1ba74e34bab2720e01785e267774a4
techno absurdist <92827957+technoabsurdist@users.noreply.github.com>
2024-01-30 21:47:42 +0100

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -75,7 +75,7 @@ export function Home() {
                   <li>ML Security Research in NeurIPS 2022 <a href="https://arxiv.org/abs/2206.10673v1" target="_blank">(Natural Backdoor Datasets)</a></li>
                   <li>Liquidity pools and and rewards feature at <a href="https://app.impossible.finance/rewards" target="_blank">Impossible Finance</a></li>
                   <li>Content automation and optimization scripts for video platforms at <a href="https://glip.gg" target="_blank">Glip</a></li>
-                  <li>Founding tutor. Taught calculus to 200+ students at <a href="https://schoolhouse.world/" target="_blank">schoolhouse.world</a></li>
+                  <li>Founding tutor. Taught calculus/cs to 200+ students at <a href="https://schoolhouse.world/" target="_blank">schoolhouse.world</a></li>
                 </ul>
                 <p className="-mb-2">
                   Some notable personal projects I've built: 
