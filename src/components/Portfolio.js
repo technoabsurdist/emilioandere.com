8357649e60786565c94c7f68c715c86c72a83787
Emilio Andere <andere@uchicago.edu>
2024-04-04 09:53:25 -0500

    target blank to all links so they work

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,7 @@ import { ProjectData } from '../data/projects'
 
 function Project({ name, date, tagline, link}) {
   return (
-    <a href={link} className="flex justify-between text-secondary py-1 group">
+    <a href={link} target="_blank" className="flex justify-between text-secondary py-1 group">
       <strong className="flex-none font-medium text-gray-900 group-hover:text-indigo-600 dark:text-gray-100 dark:group-hover:text-indigo-500">{name}</strong>
       <p className="ml-auto hidden sm:inline mr-8">{tagline}</p>
       <p>{date}</p>
